1445344e7636aaf4b9980120ed6883b984c0aa99
arya.shrivastava <arya.shrivastava@kollie.ai>
2021-02-03 02:45:52 +0530

    final code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const cheerio = require('cheerio');
-// const fs = require('fs');
 const fetch = require('node-fetch');
 
 const args = process.argv.slice(2);
@@ -10,9 +9,6 @@ if(args.length != 2) {
 
 const chartUrl = args[0];
 const count = Number(args[1]);
-// let body = null;
-
-// console.log(chartUrl+ " " + count);
 
 (async () => {
     const response = await fetch(chartUrl, {
@@ -25,13 +21,46 @@ const count = Number(args[1]);
     });
     const body = await response.text();
 
-    let $ = cheerio.load(body);
+    const $ = cheerio.load(body);
 
-    let titleList = $('td[class="titleColumn"] > a').map((i, x) => $(x).attr('href'));
-    // console.log(titleList.slice(0,count).toArray());
+    const titleList = $('td[class="titleColumn"] > a').map((i, x) => $(x).attr('href'));
 
-    // console.log(body);
-    // return body;
+    const urlList = titleList.slice(0,count).toArray().map(url => "https://www.imdb.com" + url);
+
+    let imdbData = [];
+    for(let i=0; i<urlList.length; i++) {
+        let res = await fetch(urlList[i], {
+            headers: {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9,hi-IN;q=0.8,hi;q=0.7",
+            },
+            gzip: true,
+        });
+        let stream = await res.text();
+
+        let S = cheerio.load(stream);
+        // _title_, _year_, _rating_, _summary_, _duration_, _genre_
+        let title = S('div[class="title_wrapper"] > h1').text().trim();
+        let rating = Number(S('div[class="ratingValue"] > strong > span').text());
+        let summary = S('div[class="ipc-html-content ipc-html-content--base"] > div').text().trim(); // not working (TODO)
+        let duration = S('div[class="subtext"] > time').text().trim();
+        let genre = S('#titleStoryLine > div:nth-child(8) > a').text().trim();
+        if(genre === "") {
+            genre = S('#titleStoryLine > div:nth-child(10) > a').text().trim();
+        }
+        let year = Number(S('#titleYear > a').text());
+
+        imdbData.push({
+            title,
+            year,
+            rating,
+            summary,
+            duration,
+            genre
+        });
+    }
+    console.log(JSON.stringify(imdbData));
 })();
 
 
